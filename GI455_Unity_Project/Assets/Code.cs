eda43475157a7448615071619a86d5b7e0ffcc21
DESKTOP-UB7J65P\Oat-PC <wachirawut.ruan@bumail.net>
2021-01-28 13:45:04 +0700

    Send Assignment Week1

---
+++ GI455_Unity_Project/Assets/Code.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Code : MonoBehaviour
+{
+    public string ShowName;
+    public GameObject EText;
+    public GameObject SShow;
+    
+
+    
+    public void Showname()
+    {
+        ShowName = EText.GetComponent<Text>().text;
+        SShow.GetComponent<Text>().text =  "เจอ  "+ShowName;
+    }
+}
